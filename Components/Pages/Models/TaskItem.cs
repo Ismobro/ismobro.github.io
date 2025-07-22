65b9f7f2680ecbf314d87eb489cd37306f55d648
Ismobro <ismaeelmoskinzada10@gmail.com>
2025-07-22 10:58:34 -0500

    Actual Blazor code

---
+++ Components/Pages/Models/TaskItem.cs
@@ -0,0 +1,24 @@
+﻿namespace FocusNest.Components.Pages.Models
+
+{
+    public class TaskItem
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        // string? FocusLevel { get; set; }  // e.g., "low", "medium", "high"
+        public bool IsDone { get; set; } = false;
+        public List<string> Subtasks { get; set; } = new();
+
+        public enum FocusLevels
+        {
+            Low, Medium, High
+        }
+
+        public FocusLevels FocusLevel { get; set; }
+
+
+    }
+
+
+
+}
